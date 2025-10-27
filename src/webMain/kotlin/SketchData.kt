b5be7bceac18be6b1f3e71289b04f90e604273f4
Nigel Fletton <nigel@thebluehut.com>
2025-10-27 16:24:52 -0300

    Refactor `SketchData` to compute `funcName` dynamically and remove redundant properties

--- src/webMain/kotlin/SketchData.kt
+++ src/webMain/kotlin/SketchData.kt
@@ -1,14 +1,34 @@
+private val FUNCTION_NAME_JS_REGEX: Regex =
+    Regex("""function\s*\(\)\s*\{\s*(\w+)\(\);\s+return""")
+
 internal data class SketchData(
     val navTitle: String,
     val title: String,
     val function: () -> Unit,
-    val funcName: String,
     val pkg: Package,
     val docLink: String,
     val status: SketchStatus = SketchStatus.BROKEN,
     val comment: String = "",
 ) {
-    val funcId: String = "$pkg-${funcName}".lowercase()
+    private val functionString: String = function.toString()
 
-    val codeLink: String = "$EXAMPLES_ROOT/${pkg.toString().lowercase()}/${funcName}.kt"
+    val funcName: String by lazy {
+        if (functionString.startsWith("function ()")) {
+            // JavaScript
+            FUNCTION_NAME_JS_REGEX.find(functionString)?.groupValues?.getOrNull(1)
+                ?: "unknown"
+        } else {
+            // WASM
+            val raw = functionString.substringBefore('$')
+            raw.ifEmpty { "unknown" }
+        }
+    }
+
+    val funcId: String by lazy {
+        "$pkg-$funcName".lowercase()
+    }
+
+    val codeLink: String by lazy {
+        "$EXAMPLES_ROOT/${pkg.toString().lowercase()}/$funcName.kt"
+    }
 }
