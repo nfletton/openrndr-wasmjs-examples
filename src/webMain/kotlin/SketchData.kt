4526d238006757c8232214dcac196f749c6c1edd
Nigel Fletton <nigel@thebluehut.com>
2025-10-28 11:55:15 -0300

    Rollback computed sketch funcName as it fails in production JS name mangling. The approach is too fragile

--- src/webMain/kotlin/SketchData.kt
+++ src/webMain/kotlin/SketchData.kt
@@ -1,28 +1,14 @@
-private val FUNCTION_NAME_JS_REGEX: Regex =
-    Regex("""function\s*\(\)\s*\{\s*(\w+)\(\);\s+return""")
 
 internal data class SketchData(
     val navTitle: String,
     val title: String,
     val function: () -> Unit,
+    val funcName: String,
     val pkg: Package,
     val docLink: String,
     val status: SketchStatus = SketchStatus.BROKEN,
     val comment: String = "",
 ) {
-    private val functionString: String = function.toString()
-
-    val funcName: String by lazy {
-        if (functionString.startsWith("function ()")) {
-            // JavaScript
-            FUNCTION_NAME_JS_REGEX.find(functionString)?.groupValues?.getOrNull(1)
-                ?: "unknown"
-        } else {
-            // WASM
-            val raw = functionString.substringBefore('$')
-            raw.ifEmpty { "unknown" }
-        }
-    }
 
     val funcId: String by lazy {
         "$pkg-$funcName".lowercase()
